e65bfe3120cfe2c7cdffbbc1b7a930a8768e6ec8
musho777 <129147695+musho777@users.noreply.github.com>
2024-05-04 05:11:49 +0400

    carusel

--- src/components/CateogryList/index.jsx
+++ src/components/CateogryList/index.jsx
@@ -1,75 +1,58 @@
 "use client"
 
 import CategoryItem from '../categoriItem'
-import React, { useRef, useState } from 'react';
+
 import './styles.css';
-// import soundFile from "../../assets/voice/RPReplay_Final1714777134.mp3";
 
-function CateogryList() {
-  const carouselRef = useRef(null);
-  const firstImgRef = useRef(null);
-  const [isDragStart, setDragStart] = useState(false);
-  const [isDragging, setDragging] = useState(false);
-  const [prevPageX, setPrevPageX] = useState(0);
-  const [prevScrollLeft, setPrevScrollLeft] = useState(0);
-  const [positionDiff, setPositionDiff] = useState(0);
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-  const handleDragStart = (e) => {
-    setDragStart(true);
-    setPrevPageX(e.pageX || e.touches[0].pageX);
-    setPrevScrollLeft(carouselRef.current.scrollLeft);
-  };
+const CateogryList = ({ children }) => {
 
-  const handleDragging = (e) => {
-    if (!isDragStart) return;
-    // e.preventDefault();
-    setDragging(true);
-    carouselRef.current.classList.add("dragging");
-    setPositionDiff((e.pageX || e.touches[0].pageX) - prevPageX);
-    carouselRef.current.scrollLeft = prevScrollLeft - positionDiff;
-    // showHideIcons();
-  };
-
-  const handleDragStop = () => {
-    setDragStart(false);
-    carouselRef.current.classList.remove("dragging");
-
-    if (!isDragging) return;
-    setDragging(false);
-    autoSlide();
-  };
-
-  const autoSlide = () => {
-    // const sound = new Audio(soundFile);
-    // sound.play();
-    if (carouselRef.current.scrollLeft - (carouselRef.current.scrollWidth - carouselRef.current.clientWidth) > -1 || carouselRef.current.scrollLeft <= 0) return;
-    let positionDiffAbs = Math.abs(positionDiff);
-    let firstImgWidth = firstImgRef.current.clientWidth + 50;
-    // let firstImgWidth = 120
-
-    let valDifference = firstImgWidth - positionDiffAbs;
-    if (carouselRef.current.scrollLeft > prevScrollLeft) {
-      return carouselRef.current.scrollLeft += positionDiffAbs > firstImgWidth / 3 ? valDifference : -positionDiffAbs;
-    }
-    carouselRef.current.scrollLeft -= positionDiffAbs > firstImgWidth / 3 ? valDifference : -positionDiffAbs;
+  const responsive = {
+    0: { items: 2 },
+    375: { items: 3 },
+    460: { items: 3 },
+    568: { items: 4 },
+    1024: { items: 6 },
+    1440: { items: 8 },
 
   };
+  const items = [
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+    <CategoryItem title={"Кухня 1"} />,
+  ];
+
+
 
   return (
-    <div className="wrapper">
-      <div className="carousel" ref={carouselRef} onMouseDown={handleDragStart} onTouchStart={handleDragStart} onMouseMove={handleDragging} onTouchMove={handleDragging} onMouseUp={handleDragStop} onTouchEnd={handleDragStop}>
-        <CategoryItem title={"Гостиная10"} ref={firstImgRef} />
-        <CategoryItem title={"Гостиная2"} />
-        <CategoryItem title={"Гостиная3"} />
-        <CategoryItem title={"Гостиная4"} />
-        <CategoryItem title={"Гостиная5"} />
-        <CategoryItem title={"Гостиная6"} />
-        <CategoryItem title={"Гостиная7"} />
-        <CategoryItem title={"Гостиная8"} />
-
-      </div>
+    <div className='CateogryList'>
+      <AliceCarousel
+        mouseTracking
+        items={items}
+        paddingLeft={0}
+        paddingRight={20}
+        responsive={responsive}
+        disableButtonsControls
+        disableDotsControls
+      />
     </div>
   );
-}
+};
 
 export default CateogryList;
+
--- src/components/categoriItem/index.jsx
+++ src/components/categoriItem/index.jsx
@@ -1,17 +1,10 @@
-import { forwardRef, useEffect } from 'react';
 import './styles.css'
 import { KithcenSvg } from '@/assets/Svg'
-const CategoryItem = forwardRef((props, ref) => {
-  useEffect(() => {
-    if (ref && ref.current) {
-      const width = ref.current.offsetWidth;
-    }
-  }, [ref]);
-
-  return <div className='CategoriItem' ref={ref}>
+const CategoryItem = ({ title }) => {
+  return <div className='CategoriItem' >
     <KithcenSvg className="CategoriSvg" />
-    <p id="CateogriTitle" className='Jost300_24'>{props.title}</p>
+    <p id="CateogriTitle" className='Jost300_24'>{title}</p>
   </div>
-})
+}
 CategoryItem.displayName = 'CategoryItem';
 export default CategoryItem
